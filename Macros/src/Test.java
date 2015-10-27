9d17f02211a3ccaee904f0cc2828749fb0223adc
Tyler Sedlar <sedlarizona@gmail.com>
2015-10-27 12:25:20 -0700

    re-enabled Login random

--- Macros/src/Test.java
+++ Macros/src/Test.java
@@ -1,15 +1,15 @@
 import rs.macro.api.Macro;
 import rs.macro.api.Manifest;
-import rs.macro.api.access.Bank;
-import rs.macro.api.access.Inventory;
+import rs.macro.api.access.RuneScape;
 import rs.macro.api.util.Random;
 import rs.macro.api.util.Renderable;
 import rs.macro.api.util.fx.MousePaint;
+import rs.macro.api.util.fx.Palettes;
 import rs.macro.api.util.fx.PixelOperator;
-import rs.macro.api.util.fx.Text;
 import rs.macro.api.util.fx.listener.PixelListener;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 /**
  * @author Tyler Sedlar
@@ -24,25 +24,25 @@ public class Test extends Macro implements Renderable, PixelListener {
     private static final Color MOUSE_WAVE = new Color(240, 245, 45);
     private static final Color MOUSE_TRAIL = new Color(200, 85, 70);
 
-    private int[] bankIds = new int[Bank.SLOTS.length];
-    private int[] invIds = new int[Inventory.SLOTS.length];
+    private BufferedImage image = null;
 
     @Override
     public void atStart() {
+        BufferedImage game = RuneScape.fullImage();
+        image = new BufferedImage(game.getWidth(), game.getHeight(), game.getType());
     }
 
     @Override
     public int loop() {
-        for (int i = 0; i < bankIds.length; i++) {
-            bankIds[i] = Bank.idAt(i);
+        int width = RuneScape.image().getWidth(), height = RuneScape.image().getHeight();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y,
+                        Palettes.selectFromColorBlind(RuneScape.rgbAt(x, y)));
+            }
         }
-        for (int i = 0; i < invIds.length; i++) {
-            invIds[i] = Inventory.idAt(i);
-        }
-        return Random.nextInt(50, 100);
+        return Random.nextInt(5, 10);
     }
-
-    private static final Color SHADE = new Color(40, 40, 40, 100);
 
     @Override
     public void render(Graphics2D g) {
@@ -51,28 +51,7 @@ public class Test extends Macro implements Renderable, PixelListener {
         MousePaint.drawMouseWaves(g, MOUSE_WAVE);
         MousePaint.drawTrail(g, MOUSE_TRAIL);
         MousePaint.drawOval(g, MOUSE_OUTER, MOUSE_INNER);
-        for (int i = 0; i < bankIds.length; i++) {
-            Rectangle bounds = Bank.SLOTS[i];
-            g.setColor(SHADE);
-            g.fillRect(bounds.x, bounds.y + 8, bounds.width, 15);
-            String text = Integer.toString(bankIds[i]);
-            int textWidth = g.getFontMetrics().stringWidth(text);
-            Text.drawRuneString(g, text, (bounds.x + (bounds.width / 2)) - (textWidth / 2),
-                    bounds.y + 20, Color.YELLOW);
-            g.setColor(Color.YELLOW);
-//            g.draw(bounds);
-        }
-        for (int i = 0; i < bankIds.length; i++) {
-            Rectangle bounds = Bank.SLOTS[i];
-            g.setColor(SHADE);
-            g.fillRect(bounds.x, bounds.y + 8, bounds.width, 15);
-            String text = Integer.toString(bankIds[i]);
-            int textWidth = g.getFontMetrics().stringWidth(text);
-            Text.drawRuneString(g, text, (bounds.x + (bounds.width / 2)) - (textWidth / 2),
-                    bounds.y + 20, Color.YELLOW);
-            g.setColor(Color.YELLOW);
-//            g.draw(bounds);
-        }
+        g.drawImage(image, 0, 0, null);
     }
 
     @Override
